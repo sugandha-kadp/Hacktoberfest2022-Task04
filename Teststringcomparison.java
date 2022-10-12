066239397e362bd7466815db8fbf1d6d3c55b538
uththaraHansamali <92640862+uththaraHansamali@users.noreply.github.com>
2022-10-12 10:13:09 +0530

    Update Teststringcomparison.java

--- Teststringcomparison.java
+++ Teststringcomparison.java
@@ -1,5 +1,5 @@
 package HacktoberFest2022;
-publc class Teststringcomparison {
+public class Teststringcomparison {
 	 public static void main(String args[]){  
 	   String s1="abcd";  
 	   String s2="efjh";  
