8f436457fa3fce6860de27529bbfdce28e70d136
Pranav <109653505+pran9v@users.noreply.github.com>
2022-10-19 21:08:37 +0530

    Update Teststringcomparison.java
    Syntax error fixed.

--- Teststringcomparison.java
+++ Teststringcomparison.java
@@ -3,8 +3,7 @@ package HacktoberFest2022;
 public class Teststringcomparison {
 	 public static void main(String args[]){  
 		 
-	   String s1="abcd"  
-	   
+	   String s1="abcd";
 	   String s2="efjh";  
 	   
 	   System.out.println(s1.equals(s2));  
